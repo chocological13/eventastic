3c0e83b01f864b767bd48d4f53ccc8a797762987
nina <annisamustofa13@gmail.com>
2024-06-23 18:52:42 +0700

    docs: continuing WIP authentication, SecurityConfig done

--- src/main/java/com/miniproject/eventastic/config/SecurityConfig.java
+++ src/main/java/com/miniproject/eventastic/config/SecurityConfig.java
@@ -1,10 +1,35 @@
 package com.miniproject.eventastic.config;
 
+import com.miniproject.eventastic.auth.service.impl.UserDetailsServiceImpl;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +37,71 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @RequiredArgsConstructor
 @Log
 public class SecurityConfig {
+
+  private final RsaKeyConfigProperties rsaKeyConfigProperties;
+  private final UserDetailsServiceImpl userDetailsService;
+
+  // password encoder
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  // authentication manager
+  @Bean
+  public AuthenticationManager authenticationManager() {
+    var authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return new ProviderManager(authProvider);
+  }
+
+  // jwt encoder and decoder
+  @Bean
+  public JwtEncoder jwtEncoder() {
+    // make RSA JWK
+    JWK rsaJwk =
+        new RSAKey.Builder(rsaKeyConfigProperties.publicKey()).privateKey(rsaKeyConfigProperties.privateKey()).build();
+    // define JWK Set
+    JWKSet jwkSet = new JWKSet(rsaJwk);
+
+    // make JWK source and return the encoder
+    JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(jwkSet);
+    return new NimbusJwtEncoder(jwkSource);
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder.withPublicKey(rsaKeyConfigProperties.publicKey()).build();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        // * disables unused configs
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        // * endpoints authorization
+        .authorizeHttpRequests(auth -> {
+          auth.requestMatchers("/error/**").permitAll();
+          auth.requestMatchers("/api/v1/auth/**").permitAll();
+          // ! TODO: dev purposes, delete when not used
+          auth.requestMatchers("/api/**").permitAll();
+
+          // ! TODO: add roles related authorizations
+
+          // ! TODO: uncomment this when all the security set up is ready
+          auth.anyRequest().authenticated();
+        })
+        // * session management
+        .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        // * oauth2 resource server to validate jwt
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder())))
+        // * configuring UserDetailsService, we will pass the one we already made
+        .userDetailsService(userDetailsService)
+        // * basic http authentication
+        .httpBasic(Customizer.withDefaults())
+        .build();
+  }
 
 }
