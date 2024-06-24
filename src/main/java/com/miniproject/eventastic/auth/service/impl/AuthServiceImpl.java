7022436e53d5c1b7a87f41e4fda0b4fb9b49b408
nina <annisamustofa13@gmail.com>
2024-06-24 15:49:33 +0700

    feat(auth): configured AuthService and AuthServiceImpl

---
+++ src/main/java/com/miniproject/eventastic/auth/service/impl/AuthServiceImpl.java
@@ -0,0 +1,54 @@
+package com.miniproject.eventastic.auth.service.impl;
+
+import com.miniproject.eventastic.auth.repository.AuthRedisRepository;
+import com.miniproject.eventastic.auth.service.AuthService;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+  private final AuthRedisRepository authRedisRepository;
+  private final JwtEncoder jwtEncoder;
+
+
+  @Override
+  public String generateToken(Authentication authentication) {
+
+    // for iat later
+    Instant now = Instant.now();
+
+    // define scope
+    String scope = authentication.getAuthorities()
+        .stream()
+        .map(GrantedAuthority::getAuthority)
+        .collect(Collectors.joining(" "));
+
+    // jwt claims
+    JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+        .issuer("self")
+        .issuedAt(now)
+        .expiresAt(now.plus(1, ChronoUnit.HOURS))
+        .subject(authentication.getName())
+        .claim("scope", scope)
+        .build();
+
+    // encode jwt
+    var jwt = jwtEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
+
+    // save in redis
+    authRedisRepository.saveJwtKey(authentication.getName(), jwt);
+
+    // return
+    return jwt;
+  }
+}
