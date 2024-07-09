34a16e7fbc439ce1ae81515049320aa2977b7de7
nina <annisamustofa13@gmail.com>
2024-07-09 14:36:08 +0700

    fix(cors): added pathing to /api/v1

--- src/main/java/com/miniproject/eventastic/config/CorsConfigurationSourceImpl.java
+++ src/main/java/com/miniproject/eventastic/config/CorsConfigurationSourceImpl.java
@@ -9,14 +9,19 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 public class CorsConfigurationSourceImpl implements CorsConfigurationSource {
+
   @Override
   public CorsConfiguration getCorsConfiguration(@NonNull HttpServletRequest request) {
     CorsConfiguration corsConfiguration = new CorsConfiguration();
     corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-    corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:8080", "https://eventastic-ol7zwytd3q-as.a.run.app"));
+    corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:8080", "https://eventastic-ol7zwytd3q-as.a"
+                                                                                + ".run.app", "https://eventastic"
+                                                                                              + "-ol7zwytd3q-as.a.run"
+                                                                                              + ".app/api/v1"));
     corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
     corsConfiguration.setAllowCredentials(true);
-    corsConfiguration.setExposedHeaders(List.of("Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+    corsConfiguration.setExposedHeaders(
+        List.of("Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
     return corsConfiguration;
   }
 }
